03f08eaa0f4aab7b9bd6550afd3f1ea02924f557
YogyerekDEV <mszebi2008@gmail.com>
2026-05-12 20:36:50 +0200

    dev(main): implement index.php and navbar

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles/style.css">
+    <title>Blog</title>
+</head>
+<body>
+    <div class="navbar">
+        <div class="navbar-logo">
+            <img src="resources/logo.svg" alt="logo" width="55" height="55">
+        </div>
+        <div class="navbar-buttons-container">
+            <div class="navbar-button navbar-button-selected"><a href="/Blog-School-Project/index.php">Kezdőlap</a></div>
+            <div class="navbar-button"><a href="/aboutme">Rólunk</a></div>
+        </div>
+        <div class="navbar-logo">
+            <img src="resources/user.svg" alt="logo" width="55" height="55">
+        </div>
+    </div>
+</body>
+</html>
